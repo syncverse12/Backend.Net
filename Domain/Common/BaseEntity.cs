7f9266555e6a0d1a2d2acaf698a36aa93b71b7d8
Nada Eslam <nade62204@gmail.com>
2026-01-07 09:25:31 +0200

    BaseEntity

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Graduation_Project.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string? CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string? UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
--- Infrastructure/Data/DatabaseDbContext.cs
+++ Infrastructure/Data/DatabaseDbContext.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using Graduation_Project.Domain.Common;
 using Graduation_Project.Domain.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Graduation_Project.Infrastructure.Data
 {
@@ -15,7 +16,23 @@ namespace Graduation_Project.Infrastructure.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                if (typeof(BaseEntity).IsAssignableFrom(entityType.ClrType))
+                {
+                    var parameter = System.Linq.Expressions.Expression.Parameter(entityType.ClrType, "e");
+                    var propertyMethodInfo = typeof(EF).GetMethod("Property")?.MakeGenericMethod(typeof(bool));
+                    var isDeletedProperty = System.Linq.Expressions.Expression.Call(null, propertyMethodInfo!, parameter, System.Linq.Expressions.Expression.Constant("IsDeleted"));
+                    var compareExpression = System.Linq.Expressions.Expression.MakeBinary(System.Linq.Expressions.ExpressionType.Equal, isDeletedProperty, System.Linq.Expressions.Expression.Constant(false));
+                    var lambda = System.Linq.Expressions.Expression.Lambda(compareExpression, parameter);
+
+                    builder.Entity(entityType.ClrType).HasQueryFilter(lambda);
+                }
+            }
+
             builder.ApplyConfigurationsFromAssembly(typeof(DatabaseDbContext).Assembly);
         }
+
     }
 }
