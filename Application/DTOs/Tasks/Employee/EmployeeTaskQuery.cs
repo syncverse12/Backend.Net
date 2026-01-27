b92e5eec1b559d08eced4b7c2df88b97d089f4ec
mohamed22-m-m-e <modyshabara6@gmail.com>
2026-01-27 16:36:04 +0200

    Adding EmployeeTaskQuery Class

---
+++ Application/DTOs/Tasks/Employee/EmployeeTaskQuery.cs
@@ -0,0 +1,13 @@
+﻿using Graduation_Project.Domain.Enums;
+
+namespace Graduation_Project.Application.DTOs.Tasks.Employee
+{
+    public class EmployeeTaskQuery
+    {
+        public TaskStatus? Status { get; set; }
+        public TaskPriority? Priority { get; set; }
+        public DateTime? FromDeadline { get; set; }
+        public DateTime? ToDeadline { get; set; }
+    }
+
+}
