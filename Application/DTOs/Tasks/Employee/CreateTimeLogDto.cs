189bf4fb58f22e2a3f0f9f2b4ac1aef06e0507ce
mohamed22-m-m-e <modyshabara6@gmail.com>
2026-01-27 16:35:00 +0200

    Adding CreateTimeLogDto Class

---
+++ Application/DTOs/Tasks/Employee/CreateTimeLogDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Graduation_Project.Application.DTOs.Tasks.Employee
+{
+    public class CreateTimeLogDto
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsManual { get; set; }
+    }
+
+}
