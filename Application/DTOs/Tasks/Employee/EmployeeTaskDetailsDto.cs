1719f811a97c842cfd5f4a2ec7790e0c5dfbdebe
mohamed22-m-m-e <modyshabara6@gmail.com>
2026-01-27 16:32:03 +0200

    Adding EmployeeTaskDetailsDto Class

---
+++ Application/DTOs/Tasks/Employee/EmployeeTaskDetailsDto.cs
@@ -0,0 +1,20 @@
+﻿using Graduation_Project.Domain.Enums;
+
+namespace Graduation_Project.Application.DTOs.Tasks.Employee
+{
+    public class EmployeeTaskDetailsDto
+    {
+        public string TaskId { get; set; } = null!;
+        public string TaskTitle { get; set; } = null!;
+        public string? Description { get; set; }
+
+        public TaskStatus Status { get; set; }
+        public TaskPriority Priority { get; set; }
+
+        public DateTime? Deadline { get; set; }
+        public int ProgressPercentage { get; set; }
+
+        public string ProjectName { get; set; } = null!;
+    }
+
+}
