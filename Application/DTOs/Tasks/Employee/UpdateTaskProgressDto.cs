64e2c7d21a8f6d38a4e8263287ed494e5e0bb7ff
mohamed22-m-m-e <modyshabara6@gmail.com>
2026-01-27 16:33:16 +0200

    Adding UpdateTaskProgressDto Class

---
+++ Application/DTOs/Tasks/Employee/UpdateTaskProgressDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Graduation_Project.Application.DTOs.Tasks.Employee
+{
+    public class UpdateTaskProgressDto
+    {
+        public int ProgressPercentage { get; set; }
+        public TaskStatus Status { get; set; }
+    }
+
+}
