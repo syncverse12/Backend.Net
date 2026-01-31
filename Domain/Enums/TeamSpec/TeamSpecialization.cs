d414a9847c9a16eda79740517aa07b62dd681c41
mohamed22-m-m-e <modyshabara6@gmail.com>
2026-01-31 17:22:55 +0200

    Adding Team Specializations Enum

---
+++ Domain/Enums/TeamSpec/TeamSpecialization.cs
@@ -0,0 +1,13 @@
+﻿namespace Graduation_Project.Domain.Enums.TeamSpec
+{
+    public enum TeamSpecialization
+    {
+        Backend = 1,
+        Frontend = 2,
+        Mobile = 3,
+        FullStack = 4,
+        DevOps = 5,
+        QA = 6
+    }
+
+}
