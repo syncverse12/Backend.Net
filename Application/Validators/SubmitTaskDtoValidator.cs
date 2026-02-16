a6a8dbfb317544d8257acac68b4b58794c3046e9
mohamed22-m-m-e <modyshabara6@gmail.com>
2026-02-16 05:58:09 +0200

    Adding Submission Links Validator

---
+++ Application/Validators/SubmitTaskDtoValidator.cs
@@ -0,0 +1,35 @@
+using FluentValidation;
+using Graduation_Project.Application.DTOs.Tasks.Employee;
+
+namespace Graduation_Project.Application.Validators
+{
+    public class SubmitTaskDtoValidator : AbstractValidator<SubmitTaskDto>
+    {
+        public SubmitTaskDtoValidator()
+        {
+            RuleFor(x => x)
+                .Must(dto => !string.IsNullOrWhiteSpace(dto.SubmissionLink) || 
+                             !string.IsNullOrWhiteSpace(dto.SubmissionNotes))
+                .WithMessage("Must Add Submission Link Or Notes at least");
+
+            RuleFor(x => x.SubmissionLink)
+                .Must(BeAValidUrl)
+                .When(x => !string.IsNullOrWhiteSpace(x.SubmissionLink))
+                .WithMessage("Invalid Link, Please Upload a Vaild Link");
+
+            RuleFor(x => x.SubmissionNotes)
+                .MaximumLength(1000)
+                .When(x => !string.IsNullOrWhiteSpace(x.SubmissionNotes))
+                .WithMessage("Notes Must Not Exceed 1000 Letters");
+        }
+
+        private bool BeAValidUrl(string? url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                return true;
+
+            return Uri.TryCreate(url, UriKind.Absolute, out var uriResult) &&
+                   (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+        }
+    }
+}
